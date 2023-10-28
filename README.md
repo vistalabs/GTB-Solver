0b156cf9e228458ff16f0c3470be3d4972ebda60
Kyle Rush <kwrush024@pccsk12.com>
2023-10-28 03:20:11 +0000

    Algorithm now checks the space count in words

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# GTB-Solver !!gtb.yangciou.com is no longer being updated!!
-## To Use This:
-- Download the source code
-- Open index.html in a browser
-- Type in hints given by the game and possible results will show up
-- You can add onto the words in wordlist/wordlist.js (words from the end of the submissions.txt aren't added yet)
-- The original words were scraped using a bot
+# Quackings GTB-Solver
+No new words, but better theme and search.
--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,59 @@
 // Regex Search
 $(document).ready(function () {
-      var wordsArray = arrayData;
-     
-      $("[name='username']").keyup(function () {
-        var words = $(this).val().replace(/_/g, '.');
+  var wordsArray = arrayData;
+  var spacesCount = spaces
+  
+  $("[name='username']").keyup(function () {
+    var words = $(this).val().replace(/_/g, '.');
+    
+      // console.log(words);
+      try {
+        var searchWord = words.toLowerCase()
+        var numSpaces = searchWord.split(" ").length - 1;
         
-          console.log(words);
-          try {
-            var searchWord = words.toLowerCase()
+        var reg = new RegExp(searchWord);
+        $("#result").html('');
+        for (var j = 0; j < wordsArray.length; j++) {
+          var value = wordsArray[j];
+          var eachWord = value.toLowerCase()
 
-            var reg = new RegExp(searchWord);
-            $("#result").html('');
-            for (var j = 0; j < wordsArray.length; j++) {
-              var value = wordsArray[j];
-              var eachWord = value.toLowerCase()
+          if (reg.test(eachWord)) {
 
-              if (reg.test(eachWord)) {
+            var i;
+            if (searchWord.length != eachWord.length)
+              continue;
+            for (i = 0; i < searchWord.length; i++) {
+              if (searchWord[i] == '.')
+                continue;
+              if (searchWord[i] != eachWord[i])
+                break;
 
-                var i;
-                if (searchWord.length != eachWord.length)
-                  continue;
-                for (i = 0; i < searchWord.length; i++) {
-                  if (searchWord[i] == '.')
-                    continue;
-                  if (searchWord[i] != eachWord[i])
-                    break;
+            }
 
-                }
-                if (i == searchWord.length) // TODO: fix this to exclude spaces in answers
-                  $("#result").append(value + '<br>');
-              }
+            if (i == searchWord.length && spaces[j] == numSpaces){
+              $("#result").append(value + '<br>');
             }
           }
-          catch (e) {
-            // regular expression error
-          }
-        
-      });
-    });  
+        }
+      }
+      catch (e) {
+        // regular expression error
+      }
+    
+  });
+});  
 
 // Help Modal
-  var helpModal = document.getElementById("help-modal");
-  var helpBtn = document.getElementById("help-btn");
-  var helpCloseBtn = document.getElementById("help-modal-close");
+// var helpModal = document.getElementById("help-modal");
+// var helpBtn = document.getElementById("help-btn");
+// var helpCloseBtn = document.getElementById("help-modal-close");
 
-  helpBtn.addEventListener("click", function() {
-    helpModal.style.display = "block";
-  });
-  helpCloseBtn.addEventListener("click", function() {
-    helpModal.style.display = "none";
-  });
+// helpBtn.addEventListener("click", function() {
+//   helpModal.style.display = "block";
+// });
+// helpCloseBtn.addEventListener("click", function() {
+//   helpModal.style.display = "none";
+// });
 
 
 // Character Count
